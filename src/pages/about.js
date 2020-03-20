ade2768922a2e0d22db45384ff9ebb9f8eb6664c
mdfaizan7 <faizaanazim@gmail.com>
2020-03-20 14:47:12 +0530

    made author and single author pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,11 +5,25 @@ import SEO from "../components/seo"
 const AboutPage = () => (
   <Layout pageTitle="About Me">
     <SEO title="About" />
+    <br />
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. In sapiente
-      mollitia, praesentium beatae inventore et. Ex, hic quas. Voluptatem maxime
-      similique, ab tempora nesciunt ipsa eveniet facere odio perspiciatis
-      eligendi.
+      I am a <b>Computer Science undergrad</b> pursuing my B-Tech from{" "}
+      <b>
+        <a
+          href="http://www.bitdurg.ac.in/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Bhilai Institute of Technology, Durg.
+        </a>
+      </b>
+    </p>
+    <p>
+      I am a pretty fast learner and always intrested in new technologies. I put
+      my efforts on learning the last trends on development and strive myself as
+      a better developer. I think one of my ability is to resolve problems so
+      from the beginning I've been focusing on solving algorithms no matter what
+      language I use.
     </p>
   </Layout>
 )
--- src/pages/authors.js
+++ src/pages/authors.js
@@ -1,11 +1,51 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import authors from "../util/authors"
+import tonyImage from "../images/tony.jpeg"
+import danyImage from "../images/dany.jpg"
+import natashaImage from "../images/natasha.png"
+import { Card, CardText, CardBody, CardTitle, Button, Row } from "reactstrap"
+import { slugify } from "../util/utilityFunctions"
+
+const images = [tonyImage, natashaImage, danyImage]
+var imageNum = 0
 
 const AuthorsPage = () => (
   <Layout pageTitle="Our Authors">
-    <SEO title="Authors" />
-    <h1>Authors Page</h1>
+    <SEO title="Our Authors" keywords={[`gatsby`, `application`, `react`]} />
+    {authors.map(({ name, bio }) => (
+      <Row className="mb-4">
+        <div className="col-md-3">
+          <img
+            src={images[imageNum++]}
+            alt={`${name} profile`}
+            style={{ maxWidth: "100%", minHeight: "100%" }}
+          />
+        </div>
+        <div className="col-md-8">
+          <Card style={{ minHeight: "100%" }}>
+            <CardBody>
+              <CardTitle
+                className="text-uppercase"
+                style={{ fontSize: "1.3rem" }}
+              >
+                {name}
+              </CardTitle>
+              <CardText>{bio}</CardText>
+              <Button
+                color="primary"
+                className="text-uppercase"
+                href={`/author/${slugify(name)}`}
+              >
+                View Posts
+              </Button>
+            </CardBody>
+          </Card>
+        </div>
+        {imageNum === 3 ? (imageNum = 0) : ""}
+      </Row>
+    ))}
   </Layout>
 )
 
